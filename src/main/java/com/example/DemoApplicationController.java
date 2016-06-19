4b1596a566b752a686cd9c4d6583dc1cf362e2fe
Anil Sharma <sanil@sanil-m01.vmware.com>
2016-06-19 19:49:26 +0530

    Adding files

---
+++ src/main/java/com/example/DemoApplicationController.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+@Controller
+@RequestMapping(value = "/")
+public class DemoApplicationController {
+	
+	private static Logger LOGGER = Logger.getLogger(DemoApplicationController.class);
+	
+	@RequestMapping(method=RequestMethod.GET)
+	@ResponseBody
+	public String greetings(){
+		LOGGER.info("***************");
+		return "Hello World";
+	}
+
+}
